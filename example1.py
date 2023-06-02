384d3b049dc0f0da3ceacd4554620608cbd243c8
RuthMaya <131408401+RuthMaya@users.noreply.github.com>
2023-06-01 18:53:25 -0600

    Add files via upload

--- example1.py
+++ example1.py
@@ -7,6 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1p0C1OB4m0X_DAi5NlHmYW6hevGij2Il5
 """
 
-import streamlit as lit
+import streamlit as st
 
-st.write('Hello, World! :sunglasses:')
+st.write('Hello, *World!* :sunglasses:')
